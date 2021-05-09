9f324d24fd04d6e2df378699eb6adcfca22eab76
Nykorrin <nykorrin@gmail.com>
2021-05-09 01:22:37 -0700

    Added lockdown

--- Scanner.py
+++ Scanner.py
@@ -2,18 +2,29 @@
 # Access granted/denied DONE
 # Granted/denied audio DONE
 # Termination key DONE
-# Rasperry Pi INC.
+# System lockdown INC.
+# Raspberry Pi INC.
 # Lock connection INC.
+import vlc
+import time
+
+failed_attempts = 0
+
 
 def main():
-    import vlc
-    import time
-
     granted_audio = vlc.MediaPlayer("audio/entity_cardlock_granted.wav")
     denied_audio = vlc.MediaPlayer("audio/entity_cardlock_denied.wav")
+    # terminated_audio
+    global failed_attempts
+
     key = input("Enter authorization key: ")
 
     # Work in progress
+    if failed_attempts == 3:
+        print("System lockdown initiated")
+        time.sleep(5)
+        failed_attempts = 0
+        print("System lockdown disabled.")
     if key.startswith("%"):
         if key == "%KOKOPOP202?":
             print("Authorization granted.")
@@ -22,6 +33,7 @@ def main():
         else:
             print("Authorization denied.")
             denied_audio.play()
+            failed_attempts = failed_attempts + 1
             time.sleep(1)
     elif key == "rhydonium".lower():
         print("Terminating session.")
@@ -31,10 +43,10 @@ def main():
     else:
         print("Invalid authorization key.")
         denied_audio.play()
+        failed_attempts = failed_attempts + 1
         time.sleep(1)
-
     if key.isprintable():
-        time.sleep(5)
+        time.sleep(2)
         main()
 
 
